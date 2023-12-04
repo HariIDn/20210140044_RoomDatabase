be4a02b6c36fa973d7d39ab31b47580d98afd994
HariIDn <aldifaadit75@gmail.com>
2023-12-04 21:27:12 +0700

    menambahkan function pada class aplikasi siswa

--- app/src/main/java/com/example/kelas8/AplikasiSiswa.kt
+++ app/src/main/java/com/example/kelas8/AplikasiSiswa.kt
@@ -1,4 +1,15 @@
 package com.example.kelas8
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.kelas8.repositori.ContainerApp
+import com.example.kelas8.repositori.ContainerDataApp
+
+class AplikasiSiswa: Application() {
+    // Instance app container digunakan oleh kelas lainnya untuk dapat dependencies
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
