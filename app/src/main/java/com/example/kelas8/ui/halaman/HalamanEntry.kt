c7cbdfd4abc10db48f6bce1c868a7180d8fbc25e
HariIDn <aldifaadit75@gmail.com>
2023-12-06 23:46:38 +0700

    menambah function EntrySiswaScreen dalam file HalamanEntry.kt

--- app/src/main/java/com/example/kelas8/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/kelas8/ui/halaman/HalamanEntry.kt
@@ -4,26 +4,73 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import com.example.kelas8.R
 import com.example.kelas8.model.DetailSiswa
+import com.example.kelas8.model.EntryViewModel
 import com.example.kelas8.model.UIStateSiswa
 import com.example.kelas8.navigasi.DestinasiNavigasi
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kelas8.model.PenyediaViewModel
+import kotlinx.coroutines.CoroutineScope
 
 object DestinasiEntry: DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(id = DestinasiEntry.titleRes)
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) {innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.Launch{
+                    viewModel.saveSiswa()
+                    navigateBack
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
 }
 
 @Composable
